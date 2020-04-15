1b077e7fb7b8f6be1333b6b91df3faf01241fca3
Ysrael <yrra_rojas@hotmail.com>
2020-04-15 04:12:36 -0500

    Primer commit

---
+++ app-old.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+        res.write('Hola Mundo!!!');
+        res.end();
+    })
+    .listen(8080);
+
+console.log('Escuchando en el puerto 8080');
